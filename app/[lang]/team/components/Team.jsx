471c8a620ba0f5921565a4b865ebf2a7a8ac5555
yahyabdd <boujaddiyahya@gmail.com>
2023-11-23 12:34:24 +0100

    updated team members page

--- app/[lang]/team/components/Team.jsx
+++ app/[lang]/team/components/Team.jsx
@@ -1,90 +1,325 @@
 export default function Team({lang}) {
-    // Team members data in JSON format
+
     const teamMembersEn = [
         {
-            category: 'Managment',
-            members: [
-                {
-                    title: 'Founder & Director',
-                    name: 'Veronica Aaron',
-                    imageUrl: 'https://placehold.co/700x900',
-                    profileLink: 'artist-details.html',
-                },
-                {
-                    title: 'Salon Manager',
-                    name: 'Olivia Grosh',
-                    imageUrl: 'https://placehold.co/700x900',
-                    profileLink: 'artist-details.html',
-                },
-            ],
+        category: 'Hair',
+        members: [
+        {
+        title: 'Hair Styling Expert',
+        name: 'Fatin',
+        imageUrl: 'flaticon-hairstyle',
+        profileLink: 'artist-details.html',
         },
         {
-            category: 'Hair Stylists',
-            members: [
-                {
-                    title: 'Hair Stylist',
-                    name: 'Nichole Smith',
-                    imageUrl: 'https://placehold.co/700x900',
-                    profileLink: 'artist-details.html',
-                },
-                {
-                    title: 'Hair Stylist',
-                    name: 'Sara Mills',
-                    imageUrl: 'https://placehold.co/700x900',
-                    profileLink: 'artist-details.html',
-                },
-                {
-                    title: 'Hair Stylist',
-                    name: 'Rachel Hanley',
-                    imageUrl: 'https://placehold.co/700x900',
-                    profileLink: 'artist-details.html',
-                },
-            ],
+        title: 'Hair Styling Expert',
+        name: 'Ahlam',
+        imageUrl: 'flaticon-hairstyle',
+        profileLink: 'artist-details.html',
         },
-    ];
-
+        {
+        title: 'Hair Styling Expert',
+        name: 'Iman',
+        imageUrl: 'flaticon-hairstyle',
+        profileLink: 'artist-details.html',
+        },
+        {
+        title: 'Hair Styling Expert',
+        name: 'Manal',
+        imageUrl: 'flaticon-hairstyle',
+        profileLink: 'artist-details.html',
+        },
+        {
+        title: 'Hair Styling Expert',
+        name: 'Wafa',
+        imageUrl: 'flaticon-hairstyle',
+        profileLink: 'artist-details.html',
+        },
+        {
+        title: 'Hair Styling Expert',
+        name: 'Hoyam',
+        imageUrl: 'flaticon-hairstyle',
+        profileLink: 'artist-details.html',
+        },
+        {
+        title: 'Hair Styling Expert',
+        name: 'Reem',
+        imageUrl: 'flaticon-hairstyle',
+        profileLink: 'artist-details.html',
+        },
+        {
+        title: 'Hair Styling Expert',
+        name: 'Gina',
+        imageUrl: 'flaticon-hairstyle',
+        profileLink: 'artist-details.html',
+        },
+        ],
+        },
+        {
+        category: 'Makeup',
+        members: [
+        {
+        title: 'Makeup Artist',
+        name: 'Wafa',
+        imageUrl: 'flaticon-makeup',
+        profileLink: 'artist-details.html',
+        },
+        {
+        title: 'Makeup Artist',
+        name: 'Lina',
+        imageUrl: 'flaticon-makeup',
+        profileLink: 'artist-details.html',
+        },
+        {
+        title: 'Makeup Artist',
+        name: 'Zoya',
+        imageUrl: 'flaticon-makeup',
+        profileLink: 'artist-details.html',
+        },
+        ],
+        },
+        {
+        category: 'Nails',
+        members: [
+        {
+        title: 'Nail Artist',
+        name: 'Ahlam',
+        imageUrl: 'flaticon-nail',
+        profileLink: 'artist-details.html',
+        },
+        {
+        title: 'Nail Artist',
+        name: 'Roua',
+        imageUrl: 'flaticon-nail',
+        profileLink: 'artist-details.html',
+        },
+        {
+        title: 'Nail Artist',
+        name: 'Jessica',
+        imageUrl: 'flaticon-nail',
+        profileLink: 'artist-details.html',
+        },
+        {
+        title: 'Nail Artist',
+        name: 'Gina',
+        imageUrl: 'flaticon-nail',
+        profileLink: 'artist-details.html',
+        }
+        ],
+        },
+        {
+        category: 'Massage',
+        members: [
+        {
+        title: 'Massage Specialist',
+        name: 'Jessica',
+        imageUrl: 'flaticon-foot-massage',
+        profileLink: 'artist-details.html',
+        },
+        {
+        title: 'Massage Specialist',
+        name: 'Gina',
+        imageUrl: 'flaticon-foot-massage',
+        profileLink: 'artist-details.html',
+        }
+        ],
+        },
+        {
+        category: 'Eyebrows and eyelashes',
+        members: [
+        {
+        title: 'Eyebrows and eyelashes Artist',
+        name: 'Heba',
+        imageUrl: 'flaticon-eyelashes',
+        profileLink: 'artist-details.html',
+        },
+        {
+        title: 'Eyebrows and eyelashes Artist',
+        name: 'Hajar',
+        imageUrl: 'flaticon-eyelashes',
+        profileLink: 'artist-details.html',
+        },
+        ],
+        },
+        {
+        category: 'Body Care',
+        members: [
+        {
+        title: 'Body Care Specialist',
+        name: 'Gina',
+        imageUrl: 'flaticon-facial-treatment',
+        profileLink: 'artist-details.html',
+        },
+        ],
+        },
+        {
+        category: 'Skin Care',
+        members: [
+        {
+        title: 'Skin Care Specialist',
+        name: 'Ghadah',
+        imageUrl: 'flaticon-skin-care',
+        profileLink: 'artist-details.html',
+        },
+        ],
+        },
+        ];
     const teamMembersAr = [
         {
-            category: 'الإدارة',
-            members: [
-                {
-                    title: 'المؤسسة والمديرة',
-                    name: 'فاطمة علي',
-                    imageUrl: 'https://placehold.co/700x900',
-                    profileLink: 'artist-details.html',
-                },
-                {
-                    title: 'مديرة صالون',
-                    name: 'ليلى حسن',
-                    imageUrl: 'https://placehold.co/700x900',
-                    profileLink: 'artist-details.html',
-                },
-            ],
+        category: 'الشعر',
+        members: [
+        {
+        title: 'أخصائية تصفيف الشعر',
+        name: 'فاتن',
+        imageUrl: 'flaticon-hairstyle',
+        profileLink: 'artist-details.html',
         },
         {
-            category: 'أخصائيات تصفيف الشعر',
-            members: [
-                {
-                    title: 'أخصائية تصفيف الشعر',
-                    name: 'فاطمة عبد الله',
-                    imageUrl: 'https://placehold.co/700x900',
-                    profileLink: 'artist-details.html',
-                },
-                {
-                    title: 'أخصائية تصفيف الشعر',
-                    name: 'لمى حسين',
-                    imageUrl: 'https://placehold.co/700x900',
-                    profileLink: 'artist-details.html',
-                },
-                {
-                    title: 'أخصائية تصفيف الشعر',
-                    name: 'ريما محمد',
-                    imageUrl: 'https://placehold.co/700x900',
-                    profileLink: 'artist-details.html',
-                },
-            ],
+        title: 'أخصائية تصفيف الشعر',
+        name: 'احلام',
+        imageUrl: 'flaticon-hairstyle',
+        profileLink: 'artist-details.html',
         },
-    ];
+        {
+        title: 'أخصائية تصفيف الشعر',
+        name: 'منال',
+        imageUrl: 'flaticon-hairstyle',
+        profileLink: 'artist-details.html',
+        },
+        {
+        title: 'أخصائية تصفيف الشعر',
+        name: 'وفا',
+        imageUrl: 'flaticon-hairstyle',
+        profileLink: 'artist-details.html',
+        },
+        {
+        title: 'أخصائية تصفيف الشعر',
+        name: 'هيام',
+        imageUrl: 'flaticon-hairstyle',
+        profileLink: 'artist-details.html',
+        },
+        {
+        title: 'أخصائية تصفيف الشعر',
+        name: 'ريم',
+        imageUrl: 'flaticon-hairstyle',
+        profileLink: 'artist-details.html', 
+        },
+        {
+        title: 'أخصائية تصفيف الشعر',
+        name: 'جين',
+        imageUrl: 'flaticon-hairstyle',
+        profileLink: 'artist-details.html',
+        },
+        ],
+        },
+        {
+        category: 'المايك اب',
+        members: [
+        {
+        title: 'أخصائية مايك اب',            
+        name: 'وفا',
+        imageUrl: 'flaticon-makeup',
+        profileLink: 'artist-details.html',
+        },
+        {
+        title: 'أخصائية مايك اب',
+        name: 'لين',
+        imageUrl: 'flaticon-makeup',
+        profileLink: 'artist-details.html',
+        },
+        {
+        title: 'أخصائية مايك اب',
+        name: 'زويا',
+        imageUrl: 'flaticon-makeup',
+        profileLink: 'artist-details.html',
+        },
+        ],
+        },
+        {
+        category: 'الاظافر',
+        members: [
+        {
+        title: 'أخصائية الاظافر',
+        name: 'احلام',
+        imageUrl: 'flaticon-nail',
+        profileLink: 'artist-details.html',
+        },
+        {
+        title: 'أخصائية الاظافر',
+        name: 'روعة',
+        imageUrl: 'flaticon-nail',
+        profileLink: 'artist-details.html',
+        },
+        {
+        title: 'أخصائية الاظافر',
+        name: 'جيسيكا',
+        imageUrl: 'flaticon-nail',
+        profileLink: 'artist-details.html',
+        },
+        {
+        title: 'أخصائية الاظافر',
+        name: 'جين',
+        imageUrl: 'flaticon-nail',
+        profileLink: 'artist-details.html',
+        }
+        ],
+        },
+        {
+        category: 'مساج',
+        members: [
+        {
+        title: 'أخصائية مساج',
+        name: 'جيسيكا',
+        imageUrl: 'flaticon-foot-massage',
+        profileLink: 'artist-details.html',
+        },
+        {
+        title: 'أخصائية مساج',
+        name: 'جين',
+        imageUrl: 'flaticon-foot-massage',
+        profileLink: 'artist-details.html',
+        }
+        ],
+        },
+        {
+        category: ' العناية بالجسم',
+        members: [
+        {
+        title: 'أخصائية العناية بالجسم',
+        name: 'جين',
+        imageUrl: 'flaticon-facial-treatment',
+        profileLink: 'artist-details.html',
+        },
+        ],
+        },
+        {
+        category: 'الحواجب والرموش',
+        members: [
+        {
+        title: 'أخصائية الحواجب والرموش',
+        name: 'هاجر',
+        imageUrl: 'flaticon-eyelashes',
+        profileLink: 'artist-details.html',
+        },
+        {
+        title: 'أخصائية الحواجب والرموش',
+        name: 'هبة',
+        imageUrl: 'flaticon-eyelashes',
+        profileLink: 'artist-details.html',
+        },
+        ],
+        },
+        {
+        category: 'العناية بالبشرة',
+        members: [
+        {
+        title: 'أخصائية العناية بالبشرة',
+        name: 'غادة',
+        imageUrl: 'flaticon-skin-care',
+        profileLink: 'artist-details.html',
+        },
+        ],
+        },
+        ];
 
     const teamMembers = lang === 'en' ? teamMembersEn : teamMembersAr
     return (
@@ -103,21 +338,14 @@ export default function Team({lang}) {
                             {category.members.map((member, memberIndex) => (
                                 <div key={memberIndex} className="col">
                                     <div className="team-member wow fadeInUp">
-                                        <div className="team-member-photo">
-                                            <div className="hover-overlay">
-                                                <img
-                                                    className="img-fluid"
-                                                    src={member.imageUrl}
-                                                    alt={`team-member-foto-${memberIndex}`}
-                                                />
-                                                <div className="item-overlay" />
-                                            </div>
+                                        <div className="sbox-ico ico-65">
+                                            <span className= {`${member.imageUrl} color--black`} />
                                         </div>
                                         <div className="team-member-data">
                                             <span className="section-id">{member.title}</span>
                                             <h5 className="h5-lg">{member.name}</h5>
                                             <p className="tra-link">
-                                                <a href={`${lang}/artist`}>View Profile</a>
+                                                <a href='/booking'>Book an apointment</a>
                                             </p>
                                         </div>
                                     </div>
