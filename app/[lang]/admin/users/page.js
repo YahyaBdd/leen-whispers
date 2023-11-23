60e407d4b6b5e717fc1abee5f16f7310375668f2
yahyabdd <boujaddiyahya@gmail.com>
2023-11-23 20:16:31 +0100

    added user export feature

--- app/[lang]/admin/users/page.js
+++ app/[lang]/admin/users/page.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import { collection, getDocs } from "firebase/firestore";
 import { db } from '@/firebase';
-import { utils } from 'xlsx';
+import { utils, writeFile } from 'xlsx';
 import FileSaver from 'file-saver';
 
 export default async function index() {
@@ -12,8 +12,7 @@ export default async function index() {
         const querySnapshot = await getDocs(collection(db, "users"));
         querySnapshot.forEach((doc) => {
             // doc.data() is never undefined for query doc snapshots
-            users[doc.id] = doc.data();
-            // console.log(doc.id, " => ", doc.data());
+            users[doc.id] = doc.data();;
         });
         return users;
     }
@@ -21,30 +20,49 @@ export default async function index() {
     const users = await getUsers();
 
     const downloadUsers = () => {
-        const workbook = utils.book_new();
 
-        Object.keys(users).forEach(key => {
-            const sheet = workbook.addWorksheet(key);
-            const dataForSheet = users[key];
-
-            sheet.addRow([dataForSheet.phoneNumber?dataForSheet.phoneNumber:dataForSheet.email, dataForSheet.fullName, dataForSheet.pwd, dataForSheet.uid]);
+        const data = Object.values(users)
+        data.forEach((user, index) => {
+            if(user.pwd) {
+                delete user.pwd;
+            }
         });
+        const workbook = utils.book_new();
+        
+        const ws = utils.json_to_sheet(data);
 
-        const excelFile = workbook.write();
-
-        FileSaver.saveAs(new Blob([excelFile], {type: 'application/octet-stream'}), 'users.xlsx');
+        utils.book_append_sheet(workbook, ws, "Users");
+        writeFile(workbook, "users.xlsx");
     }
+
   return (
-    <div style={{paddingTop:"500px"}}>
-        {users && Object.keys(users).map((user, index) => (
-            <>
-                <div key={index}>{users[user].fullName}</div>
-                <p>{users[user].phoneNumber ? users[user].phoneNumber : users[user].email}</p>
-            </>
-            )
-        )
-        }
-        <button onClick={downloadUsers}>Download</button>
+    <div className="container" style={{paddingTop:"100px"}}>
+        <h1 className='text-center'>Users List</h1>
+        <br/>
+        <br/>
+        <table className='table table-striped'>
+            <thead>
+                <tr>
+                    <th>Full Name</th>
+                    <th>uid</th>
+                    <th>Phone Number</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+            {users && Object.keys(users).map((user, index) => (
+                <tr key={index}>
+                    <td>{users[user].fullName}</td>
+                    <td>{users[user].uid}</td>
+                    <td>{users[user].phoneNumber || ""}</td>
+                    <td>{users[user].email || ""}</td>
+                </tr>
+                ))
+            }
+            </tbody>
+        </table>
+        
+        <button className="btn btn--tra-black hover--black submit" onClick={downloadUsers}>Download</button>
     </div>
   )
 }
