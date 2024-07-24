4909799b565c79015e1eef472660ceae7c1ed509
yahyabdd <boujaddiyahya@gmail.com>
2024-07-24 11:42:20 +0100

    add GoogleAnalytics

--- app/[lang]/layout.js
+++ app/[lang]/layout.js
@@ -1,3 +1,5 @@
+import GoogleAnalytics from "@/components/GoogleAnalytics";
+
 export const metadata = {
   title: 'Leen Whispers',
   description: 'After several years of providing home services, and thanks to the great support, trust, and love from the distinguished ladies of the community, we decided to turn that idea into a tangible reality by creating a comprehensive salon. This salon aims to serve beautiful women, allowing them to enjoy the finest details and comprehensive care. We are committed to providing our services with love and attention, continuing to deliver the best in the ways you have come to expect from us.',
@@ -9,6 +11,7 @@ export default async function RootLayout({ children, params }) {
 
   return (
     <html  lang={params.lang} dir={direction}>
+      <GoogleAnalytics />
       <body lang={params.lang} >
         {children}
       </body>
